0cd95811e6c179fc626596514b021e42361d047c
harshjain1601 <99506773+harshjain1601@users.noreply.github.com>
2022-08-21 23:02:16 +0530

    first commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -10,7 +10,7 @@ const sql = require('mysql2');
 
 const con = sql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.PORT||3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
